a3dedaeb9711dd450d7f51d37dc43cf7c2666662
patrache <ssk5442@naver.com>
2023-05-26 20:27:23 +0900

    [fix]: fix db.config.ts can't read .env file

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,4 +1,9 @@
+import { createClient } from "redis"
+import { Client } from "redis-om"
 import { DataSource } from "typeorm" 
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export const mySqlClient = new DataSource({
     type: "mysql",
@@ -6,7 +11,7 @@ export const mySqlClient = new DataSource({
     port: +process.env.MYSQL_PORT,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_DB,
     synchronize: true,
     logging: true,
     entities: ["src/entity/*.ts"],
@@ -18,12 +23,16 @@ export const mySqlClient = new DataSource({
     ]
 })
 
+
+
 export const databaseInitialize = async () => {
     await mySqlClient.initialize()
         .then(() => {
-            console.log("Data Source has been initialized!")
+            console.log("Mysql Data Source has been initialized!")
         })
         .catch((err) => {
-            console.error("Error during Data Source initialization", err)
+            console.error("Error during Mysql Data Source initialization", err)
         })
+    
+    
 }
--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
-databaseInitialize()
+await databaseInitialize()
 
 app.use(cors())
 app.use(express.json())
